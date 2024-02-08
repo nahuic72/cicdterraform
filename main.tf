ef33edfaf0ace9d3b60b69e8949443ccb24694d5
nahuic72 <45906252+nahuic72@users.noreply.github.com>
2024-02-08 09:31:12 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "joseCompany"
+    organization = "joseCompan"
 
     workspaces {
       name = "EjemploCICD"
