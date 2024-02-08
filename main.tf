c4c5e133a972f8d21a89dd1c506559bca8d2fbd0
nahuic72 <45906252+nahuic72@users.noreply.github.com>
2024-02-08 10:26:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "joseCompan"
+    organization = "josecitonube"
 
     workspaces {
-      name = "EjemploCICD"
+      name = "apicloud"
     }
   }
 }
